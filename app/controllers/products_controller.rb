d4f4295f5b4908f53cbd05d356e66ec3002c9184
daschne8 <daschne8@gmail.com>
2019-06-26 14:52:56 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,36 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.find_or_create_by(product_params)
+    redirect_to products_path
+  end
+
+  def body
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.available?
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name,:description,:inventory)
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def available?
+    return (self.inventory > 0)
+  end
+
 end
